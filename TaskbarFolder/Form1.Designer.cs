80243263daaf069d2fd4c29aae565942f2136704
DanRotaru <dan.rotaru2000@yandex.ru>
2022-08-23 00:08:18 +0300

    first commit

---
+++ TaskbarFolder/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TaskbarFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Location = new System.Drawing.Point(874, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(110, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(37, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Spotify";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel2.Location = new System.Drawing.Point(1020, 22);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(80, 80);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.Enabled = false;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(22, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(36, 36);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(355, 180);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "app";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ TaskbarFolder/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskbarFolder
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        //public Timer timer = new Timer();
+        public int formPositionY = 0;
+
+        // Dark/light theme
+        public bool lightTheme = false;
+
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            //timer.Interval = 10;
+            //timer.Tick += OnTimer;
+            //timer.Start();
+
+            //this.TransparencyKey = (BackColor);
+            //pictureBox3.BorderStyle = BorderStyle.None;
+
+            this.Size = new Size(355, 100);
+
+            if (lightTheme)
+            {
+                BackColor = Color.FromArgb(249, 249, 249);
+            }
+            
+
+
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, this.Width, this.Height, 10, 10));
+
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(Cursor.Position.X - (Size.Width / 2), workingArea.Bottom - Size.Height - 10);
+        }
+
+        public int[] panelSize = { 80, 80 },
+                panelLocation = { 85, 10 },
+                imageLocation = { 22, 22 },
+                imageSize = { 36, 36 };
+
+        /*private void OnTimer(Object sender, EventArgs e)
+        {
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            Location = new Point(Location.X, formPositionY);
+            formPositionY -= 50;
+            if (formPositionY <= workingArea.Bottom - Size.Height - 10)
+            {
+                timer.Stop();
+            }
+        }*/
+
+        public void createApp(String icon = "", String path = "", String label = "", Boolean showLabels = false)
+        {
+
+            if (showLabels)
+            {
+                panelLocation[0] = 95;
+            }
+
+            AppN a1 = new AppN();
+
+            int x = 10;
+            for (int i = 1; i <= 4; i++)
+            {
+                x = x + panelLocation[0];
+                if (i == 1) x = 10;
+
+                Panel p = new Panel();
+                p.Size = new Size(panelSize[0], panelSize[1]);
+                p.Location = new Point(x, panelLocation[1]);
+                p.Cursor = Cursors.Hand;
+                p.Name = "isPanel-" + i;
+                //p.Tag = path;
+                p.Tag = @"D:\Software\Notepad3\Notepad3.exe";
+
+                // Display as list view
+                //p.Dock = DockStyle.Bottom;
+
+                this.Controls.Add(p);
+
+                PictureBox img = new PictureBox();
+                Icon ico = Icon.ExtractAssociatedIcon(@"D:\Software\Notepad3\Notepad3.exe");
+                img.Image = ico.ToBitmap();
+                img.Location = new Point(imageLocation[0], imageLocation[1]);
+                img.Enabled = false;
+                img.Size = new Size(imageSize[0], imageSize[1]);
+                img.SizeMode = PictureBoxSizeMode.StretchImage;
+                p.Controls.Add(img);
+
+                if(showLabels)
+                {
+                    Label l = new Label();
+                    l.Text = label;
+                    l.ForeColor = Color.White;
+                    l.Enabled = false;
+                    l.Location = new Point(0, 50);
+                    p.Controls.Add(l);
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            createApp();
+
+            foreach (Control c in this.Controls)
+            {
+                if (c is Panel)
+                {
+                    c.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, c.Width, c.Height, 10, 10));
+                    c.MouseEnter += panelMouseEnter;
+                    c.MouseLeave += panelMouseLeave;
+                    c.Click += clickEvent;
+                }
+            }
+        }
+
+        private void clickEvent(object sender, EventArgs e)
+        {
+            Panel p = sender as Panel;
+            p.BackColor = Color.Transparent;
+
+            String path = p.Tag.ToString();
+
+            if (File.Exists(path))
+            {
+                System.Diagnostics.Process.Start(path);
+                System.Windows.Forms.Application.Exit();
+            } 
+            else
+            {
+                MessageBox.Show("File " + path + " not exists!", "Error!");
+            }
+
+        }
+
+
+        private void panelMouseEnter(object sender, EventArgs e)
+        {
+            Panel p = sender as Panel;
+            if (p.Name.Contains("isPanel"))
+            {
+                if (lightTheme)
+                {
+                    p.BackColor = Color.FromArgb(230, 230, 230);
+                } 
+                else
+                {
+                    p.BackColor = Color.FromArgb(45, 45, 45);
+                }
+            }
+        }
+
+        private void panelMouseLeave(object sender, EventArgs e)
+        {
+            Panel p = sender as Panel;
+            if (p.Name.Contains("isPanel"))
+            {
+                p.BackColor = Color.Transparent;
+            }
+            
+        }
+    }
+    public class AppN
+    {
+        public int id;
+        public String name, icon, path;
+
+        public AppN()
+        {
+
+        }
+    }
+
+    class IniFile
+    {
+        string Path;
+        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public IniFile(string IniPath = null)
+        {
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName;
+        }
+
+        public string Read(string Key, string Section = null)
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+
+        public void Write(string Key, string Value, string Section = null)
+        {
+            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+        }
+
+        public void DeleteKey(string Key, string Section = null)
+        {
+            Write(Key, null, Section ?? EXE);
+        }
+
+        public void DeleteSection(string Section = null)
+        {
+            Write(null, null, Section ?? EXE);
+        }
+
+        public bool KeyExists(string Key, string Section = null)
+        {
+            return Read(Key, Section).Length > 0;
+        }
+    }
+}
