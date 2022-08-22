b8b7572c1aa673f6b5732f6df8fc8f6c5a47076d
DanRotaru <dan.rotaru2000@yandex.ru>
2022-08-23 02:26:58 +0300

    Improvements

--- TaskbarFolder/Form1.cs
+++ TaskbarFolder/Form1.cs
@@ -49,6 +49,8 @@ namespace TaskbarFolder
             
             InitializeComponent();
 
+            // System.Diagnostics.Process.Start("http://google.com");
+
             // If is not apps
             if (!ini.KeyExists("apps"))
             {
@@ -117,7 +119,7 @@ namespace TaskbarFolder
 
         public void createApp(string app = "")
         {
-            if (File.Exists(app))
+            if (File.Exists(app) || app.Contains("http"))
             {
                 //String icon = "", String path = "", String label = "", Boolean showLabels = false
                 Boolean showLabels = false;
@@ -141,9 +143,30 @@ namespace TaskbarFolder
 
                 this.Controls.Add(p);
 
+
                 PictureBox img = new PictureBox();
-                Icon ico = Icon.ExtractAssociatedIcon(@app);
-                img.Image = ico.ToBitmap();
+                if (app.StartsWith("http"))
+                {
+                    string light_base64 = "iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QAAAAAAAD5Q7t/AAAACXBIWXMAAABgAAAAYADwa0LPAAACkklEQVRYw+2Yz2tTQRDHP0lB0pPGVi/WiwUFFW9eRI8iaCFI6lUQ/wOrAY9qcxAESz0ISs+tSBQPVlDUUy16FT1ZoUVrtGr12mrj4e0j37fd7L4kbUTpwJLJfN/Mft/O/ph9sCH/mGRa8NkEHAOOAgeBXUDeYIvAe2AKeAC8AH6vF/luoAQsALWU7bPx6V5rMoPAXBNE7DZnYrQtWWAYWLE6+ESUivj/E9Pi/7+AquWzApRNzJbJjFtBPwCngYLYloC9pi2JfQA4A8xbMSZaJTVsBbpDffI+F/t18RkR+2Nj6wEqVqxys2SKVppuUl+R+63U7BC/PmOL8T0y2ret9BXTksmRnMB3SW4PlwR76PCfFPyi2DPAPZITPZeGUEmcPpohV3kruGvlDAr+ysLy1suWQmS6WL061rNVTZ8N5UgHycTtsBKwl18hTU7XWLx9Tgvz4w78huDnPHGG5LlRB35C8CnfCPWL/toRKC/6Vw+hBdG3OnCNrX2uIrS5QdBYegK4i6yL0BfRt/gIhaQmuq90yTTwCfZrAz9F3+Z4/rvovR5COpKLDlx9f/gIzYh+IEDIRTiW7aJ/c+Aa+50nDtfo/D501Ufob2yMh3yEuojKzk4eHcGFFTpc3wjuOlxPCf7SwuzDdShEBqKCXJ0qNC4/Jh3+jwTX8iML3BdslpTlB+bNtUC7JUNrF2h94reTZIG2W8iMiX0FOJmWTCxlkvmuUE/fM7GPiM+o2OMSttcamRpwuVky8VtNWIHmiQp3LfKXgX1m5JbFPgCcJbqhaIxx2rx5lAlfg56aFroGXWmHjEqR9i6Ks7QwZ0KSAy7QXIlbBc7TxGpq5WNDlmh3LZjffpIfG2aof2yYJkrVhvy/8gf4rqhW//b74AAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyMi0wOC0yMlQyMzoxNDowNSswMDowML4I/y0AAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjItMDgtMjJUMjM6MTQ6MDUrMDA6MDDPVUeRAAAAAElFTkSuQmCC";
+                    string dark_base64 = "iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QAAAAAAAD5Q7t/AAAACXBIWXMAAABgAAAAYADwa0LPAAADwElEQVRo3u2Zy29NURTGv9OodyXUu6qtECMTGvGIgce/IC1DJgbMRMOAIlHBzPMfMEVSj8RAwoBECBMSklK3HtF6JS4qys/gnMrJOvuce869p65Hv+QO9t7fWftba5+7z15rS6MYxf8NLw8jQI2k5ZLWSlomabGkBkmTgjmKkl5KeiTprqRrkm57nvejqt4DDUAX0Ed29AXPzquG8HrgFDBYhnCLr8BpYPrvEt8ODOQg3OIN0D6SwscEkYrDkKPvMnAlJXcYZ4DavMVPALpjJnwG7ASOmv6PQBPQDBTN2GGgA3geY/MSMDHPyLvEfwM6gdrAwddmfG/IRqcZewGMA8YCh2JW5GIuKxHz2vQArSHONjP+GqgLjdcB/YazNTS+Auh1zHO6UvFtDqOPMVsf8DAu+iHOPsO5b8bnB4GxaCtXfD3R3aYHmGt4SwznCzDTYW8W0W13keE0OlZiAKgvx4FTxtA3YIWDt9/wziXYPG+4uxyc1UT/Eyezip+H/4EJozOGa1+f9gS7mwz3VgzvoOF9BeZncaCLPw9dLq2eQ3yNpF5JjZmWbeTxQlKT53nfw501DuLyP1C85J9ul9lOlwNrq600AevSONBq2tu9GEi6YbjrvRKQtME8cz2Bu8NwU63AYtO+lxCRqaZdSBHFPtOelsC1c1ttTgfmmPaThAnsB2YghQNvMjjwtIQ2pwOTTftDwgR28s8pHCiWCEIY70y7zhJqVBmo8HlJSsqLS+pzEWyEpiY8byOU5gw/uYSNpLk/pnHglWm3ZHBgRgoHbO77PoHbXEKb04HHpr00gwNpziuW8zaBa+d+VNI6sKfah54EdKRZgWspolgtRLT9TYe5gqQWW82LrEBAOFtttQ6cTV2KxJ3QHIjhPjC8TQl2NxvuzRieTWgGgYZM7hKtRgwBqxy8LCnlBcN1pZRriKaUxzOvFzAdv9wXxlOg0fBsUj8IzHLYm+1Y1YWG0wQUDKcfmFZKb5wT7Y6trAeTnxLNi/c5bNni1j0z3hwEyGJjWeJDhs84jPYCK0McW9jqB6aExqcQLdFsCY2vdkQe4ERF4gPjtfi1Sosh/LLgONylxf0hG/ZPOVxaHA8cwV1a7AbGVOxAIGBijBPgF2g7gGOmvwi0AAuAT2bsMLAbeBljs5u8irtmJbKW168GvzTcX69NbpGPcaSNkbng6KfSP2wGJ+qBk+RzxTQIHKfcrbJCR4Yv+QplCC/gbwLZvrAGeV6ztsqv24SvWYezr6Kk5/JzjTvyT5V3q37NOopR/AP4CQvsiBRMMmAcAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIyLTA4LTIyVDIzOjIzOjI5KzAwOjAwQLPvXAAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMi0wOC0yMlQyMzoyMzoyOSswMDowMDHuV+AAAAAASUVORK5CYII=";
+                    
+                    if(lightTheme)
+                    {
+                        img.Image = ConvertBase64ToImage(light_base64);
+                    }
+                    else
+                    {
+                        img.Image = ConvertBase64ToImage(dark_base64);
+                    }
+                }
+                else
+                {
+                    Icon ico = Icon.ExtractAssociatedIcon(@app);
+                    img.Image = ico.ToBitmap();
+                }
+                
+                
+                
                 img.Location = new Point(imageLocation[0], imageLocation[1]);
                 img.Enabled = false;
                 img.Size = new Size(imageSize[0], imageSize[1]);
@@ -219,11 +242,12 @@ namespace TaskbarFolder
 
             string path = p.Tag.ToString();
 
-            if (File.Exists(path))
+            if (path.StartsWith("http") || File.Exists(path))
             {
                 System.Diagnostics.Process.Start(path);
                 Application.Exit();
-            } 
+            }
+
             else
             {
                 MessageBox.Show("File " + path + " not exists!", "Error!");
@@ -256,6 +280,16 @@ namespace TaskbarFolder
                 p.BackColor = Color.Transparent;
             }
             
+        }
+
+        public Image ConvertBase64ToImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            using (MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                ms.Write(imageBytes, 0, imageBytes.Length);
+                return Image.FromStream(ms, true);
+            }
         }
     }
 
